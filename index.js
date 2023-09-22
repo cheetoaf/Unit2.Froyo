239b8f3af7f5d147d8d803e7cee9ff7b55ee9f2f
cheetoaf <cheetoaf@gmail.com>
2023-09-21 23:20:07 -0700

    Updated Code

--- index.js
+++ index.js
@@ -2,18 +2,39 @@
 // customer type in vanilla,vanilla,vanilla,strawberry,coffee,coffee as input into the prompt
 // create a table that displays how many of each flavor the customer ordered (inputted)
 // Customer order input is the following: (3) vanilla, (2) coffee, and (1) strawberry
-//let flavors = prompt("What flavors would you like?", "vanilla,vanilla,vanilla,strawberry,coffee,coffee" )
-const flavors = "vanilla,vanilla,vanilla,strawberry,coffee,coffee";
-
 // display in console the quantity of each flavor listed
 //split flavors by commas into an flavors array
 
+
+let flavors = prompt("What flavors would you like?", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
+// ^^ This creates a variable that is assigned the prompt function result of the 2nd parameter
+// ^^ This Prompts the user with a question (first parameter), (Second parameter) is the pseudo user input, in a realistic environment we would
+//^^ leave the second parameter blank so user could input strings response into the prompt
+
 let flavorArray = flavors.split(',');
+// ^^ this declares a variable that is assigned an array by the .split method. The .split(',') method
+// ^^ is being used to split the flavors variable assigned pseudo user input (parameter 2) string into an array of substring based
+// ^^ on a specific delimiter. In this case, the delimiter is the (',') after .split method and is telling .split to split the string at each comma (',')
+// ^^ after splitting the inputted string at each comma, the comma is actually removed and only the subtrings are being inputted into the array
+// ^^ commas are shown in the array response because the array datatype innately utilizes commas to separate its index positions.
+// ^^ .split method is splitting flavors user input at the comma, creating an array with the split substrings, and then assigning the array to the flavorArray variable.
+
+
 console.log(flavorArray); 
 
+// This console log allows us to see the output of the newly initialized flavorArray variable
+
 const froyoOrder = {};
+// this declares an object. It is not initialized here as it has no value
     for (let i = 0; i < flavorArray.length; i++) {
+        // this is setting the iteration equal to zerom; telling the loop to run as many times as index positions exists; and adding 1 iteration after
+        // each time the loop is ran; 
+       
         console.log(flavorArray[i]);
+// This is telling console.log to print/log the value of the current position. Each iteration/index position of the array will be logged as we used [i] 
+// instead of an actual index position in the array. Using the i or iteration starts at the first index position. When the loops runs, it will move to 
+// the next index/iteration position.
+
         console.log(i);
     }
 
